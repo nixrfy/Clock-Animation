5f64880eba1d54f0c54ae83b88e9650f302a9e60
Zean <145569956+zeanxcx@users.noreply.github.com>
2024-07-29 21:03:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+setInterval(() => {
+    const time = new Date()
+    const hour = String(time.getHours()).padStart(2, '0')
+    const minute = String(time.getMinutes()).padStart(2, '0')
+    const second = String(time.getSeconds()).padStart(2, '0')
+    const clock = hour + ':' + minute + ':' + second
+
+    document.querySelector('.board').textContent = clock
+}, 1000)
