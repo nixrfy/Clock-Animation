dc637d614dfbc8041c3d2c1b19a1cd24d1b403a8
Zean <145569956+zeanxcx@users.noreply.github.com>
2024-08-29 00:28:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 setInterval(() => {
-    const time = new Date();
-    const hour = String(time.getHours()).padStart(2, '0');
-    const minute = String(time.getMinutes()).padStart(2, '0');
-    const second = String(time.getSeconds()).padStart(2, '0');
-    const clock = hour + ':' + minute + ':' + second;
+    const time = new Date(); // Mendapatkan waktu semasa dari peranti pengguna
+    const hour = String(time.getHours()).padStart(2, '0'); // Mendapatkan jam dan menambahkan 0 jika kurang dari 10
+    const minute = String(time.getMinutes()).padStart(2, '0'); // Mendapatkan minit dan menambahkan 0 jika kurang dari 10
+    const second = String(time.getSeconds()).padStart(2, '0'); // Mendapatkan saat dan menambahkan 0 jika kurang dari 10
+    const clock = hour + ':' + minute + ':' + second; // Format masa sebagai jam:minit:saat
 
-    document.querySelector('.board').textContent = clock;
+    document.querySelector('.board').textContent = clock; // Paparkan masa di laman web
 }, 1000);
