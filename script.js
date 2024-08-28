b360c40e0f221f3481c5303ecb69a203b1114c3b
Zean <145569956+zeanxcx@users.noreply.github.com>
2024-08-29 00:21:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 setInterval(() => {
-    const time = new Date()
-    const hour = String(time.getHours()).padStart(2, '0')
-    const minute = String(time.getMinutes()).padStart(2, '0')
-    const second = String(time.getSeconds()).padStart(2, '0')
-    const clock = hour + ':' + minute + ':' + second
+    const time = new Date();
+    const hour = String(time.getHours()).padStart(2, '0');
+    const minute = String(time.getMinutes()).padStart(2, '0');
+    const second = String(time.getSeconds()).padStart(2, '0');
+    const clock = hour + ':' + minute + ':' + second;
 
-    document.querySelector('.board.).textContent = clock
-}, 1000)
+    document.querySelector('.board').textContent = clock;
+}, 1000);
